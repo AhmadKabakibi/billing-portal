da01834e05f873c76ab9bd8e9eaf766ce4c6562e
Ahmad Kabakibi <ahmadkbakibi@gmail.com>
2016-07-26 17:53:48 +0200

    ftp service fetching and sweep every interval from tsconfig.json

--- services/ftpFetch.js
+++ services/ftpFetch.js
@@ -1,7 +1,9 @@
 var models = require('../models');
 var Promise = require('bluebird');
+var path = require("path");
 var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/tsconfig.json')[env];
+var ftpLocal  =path.join(__dirname,'../',config.ftp.local);
 var FTP = require('ftpimp'),
 
     //Initializes the main FTP sequence ftp will emit a ready event once the server connection has been established
@@ -18,14 +20,21 @@ var service = module.exports = {
         });
     },
     sweepFTP: function() {
-        ftp.ls(config.ftp.root, function (err, filelist) {
-            console.log('sweeping FTP server every 20 Sec!');
-            console.log(filelist);
-        });
-        setTimeout(service.sweepFTP,20000);
-    },
-    download: function () {
+        ftp.ls(config.ftp.root, function (err, fileslist) {
+            console.log('sweeping FTP server every '+ (config.ftp.interval / 1000)  / 60 + ' minutes!');
+            //console.log(fileslist)
 
+            for(var i =0;i<fileslist.length;i++){
+               console.log(config.ftp.root+'/'+fileslist[i].filename);
+                service.download(config.ftp.root+'/'+fileslist[i].filename,path.join(ftpLocal,fileslist[i].filename));
+            }
+        });
+        setTimeout(service.sweepFTP,config.ftp.interval);
+    },
+    download: function (remote_path_file,local_path_file) {
+        ftp.save([remote_path_file, local_path_file ], function (err, filename) {
+            console.log("Saved "+filename);
+        });
     },
     archive: function(){
 
