2abddafa9310b416305cb1bc34ec9906cf9b75c0
Ahmad Kabakibi <ahmadkbakibi@gmail.com>
2016-07-27 23:49:03 +0200

    ftp service ping noop cmd

--- services/ftpFetch.js
+++ services/ftpFetch.js
@@ -1,6 +1,7 @@
 var models = require('../models');
 var Promise = require('bluebird');
 var path = require("path");
+var moment    = require('moment') //timing utulity module
 var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/tsconfig.json')[env];
 var ftpLocal  =path.join(__dirname,'../',config.ftp.local);
@@ -12,21 +13,48 @@ var FTP = require('ftpimp'),
         console.log('connected to remote FTP server');
     };
 
+//@Override FTP.prototype.unlink
+/**
+ * Runs the FTP command DELE - Delete remote file
+ * @param {string} filepath - The location of the file to be deleted.
+ * @param {function} callback - The callback function to be issued.
+ * @param {boolean} runLevel - execution priority; @see {@link FTP.Queue.RunLevels}.
+ * @param {boolean} [holdQueue=false] - Prevents the queue from firing an endproc event, user must end manually
+ */
+ftp.unlink = function (filepath, callback, runLevel, holdQueue) {
+    ftp.run(ftp.unlink.raw + ' ' + filepath, function (err, data) {
+        callback.call(callback, err, data);
+    }, runLevel, holdQueue);
+};
+ftp.unlink.raw = 'DELE';
+
 var service = module.exports = {
 
-    startFTP: function(params) {
+    startFTP: function() {
         ftp.connect(function(){
             service.sweepFTP();
+            service.pingFTP();
         });
+    },
+    pingFTP:function(){
+        ftp.ping(function(err,info){
+            if(err)
+                service.startFTP();
+            console.log("FTP ping Ok: "+info);
+        });
+        setTimeout(service.pingFTP,20000);
     },
     sweepFTP: function() {
         ftp.ls(config.ftp.root, function (err, fileslist) {
             console.log('sweeping FTP server every '+ (config.ftp.interval / 1000)  / 60 + ' minutes!');
             //console.log(fileslist)
+            if(fileslist.length==0){
+                console.log("There is no new files in root "+ config.ftp.root);
+            }
 
             for(var i =0;i<fileslist.length;i++){
-               console.log(config.ftp.root+'/'+fileslist[i].filename);
-                service.download(config.ftp.root+'/'+fileslist[i].filename,path.join(ftpLocal,fileslist[i].filename));
+               console.log(path.join(config.ftp.root,fileslist[i].filename));
+                service.download(path.join(config.ftp.root,fileslist[i].filename),path.join(ftpLocal,fileslist[i].filename));
             }
         });
         setTimeout(service.sweepFTP,config.ftp.interval);
@@ -34,10 +62,16 @@ var service = module.exports = {
     download: function (remote_path_file,local_path_file) {
         ftp.save([remote_path_file, local_path_file ], function (err, filename) {
             console.log("Saved "+filename);
+            ftp.unlink(remote_path_file,function(err,file){
+                if(err){
+                    console.log("unlink error: "+err);
+                }
+                console.log("unlink deleted from ftp "+file);
+            });
         });
     },
     archive: function(){
-
+        //append date/time to the file name and upload it to the folder (Archive)
     }
 }
 
--- services/parser.js
+++ services/parser.js
@@ -2,6 +2,7 @@ var models = require('../models');
 var Promise = require('bluebird');
 var csv = require('csv-stream');
 fs = require('fs');
+var moment    = require('moment') //timing utulity module
 var path = require('path');
 
 // All of these arguments are  optional.
@@ -28,7 +29,6 @@ var csvStream = csv.createStream(options);
 
 var service = module.exports = {
     parseFile: function(filename){
-
         fs.createReadStream(filename)
             .pipe(csv.createStream(options))
             .on('data',function(data){
@@ -43,6 +43,31 @@ var service = module.exports = {
             .on('close',function(){
                 console.log('close!');
             })
+    },
+    validateFile:function(){
+        //Not valid JSON
+        //file structure does match the sample file
+    },
+    validatePO:function(){
+        /*Anyone of the below fields is empty
+            PO number
+            Partner Code
+            PO ERP Status field
+            Item Code
+            Quantity Ordered
+            If one record within the PO is bad --> reject the full PO*/
+    },
+    statusPO:function(){
 
     }
+
+   /* If the same PO number is received in the files received from ERP
+        Check if the status in the PO =
+            Open                O             ignore and do nothing
+            Changed             C             replace existing PO if it is not invoiced yet
+            BackOrdered         B             ignore and do nothing
+            Completed           X             ignore and do nothing*/
+
+
+
 }
